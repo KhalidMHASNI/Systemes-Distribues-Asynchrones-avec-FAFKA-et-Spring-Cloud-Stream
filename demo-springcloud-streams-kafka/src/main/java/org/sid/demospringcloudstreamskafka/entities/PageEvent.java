a307aee222a3101bd34e5ab14486f93c5a2fa3a2
Gamareel <kmhasni15@gmail.com>
2023-06-06 14:55:48 +0100

    pushing

--- demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/entities/PageEvent.java
+++ demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/entities/PageEvent.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Date;
-
 @Data @NoArgsConstructor @AllArgsConstructor @ToString
 public class PageEvent {
     private String name;
